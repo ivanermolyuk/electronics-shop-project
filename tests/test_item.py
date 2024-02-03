793db5986f6682b0179fb1ed61dbf0c78019975a
Ivan Ermolyuk <ivan.ermolyuk@gmail.com>
2024-02-03 14:16:16 +0300

    add docstrings HW_1

--- tests/test_item.py
+++ tests/test_item.py
@@ -6,20 +6,24 @@ from src.item import Item
 
 @pytest.fixture
 def item():
+    """Тест Класса"""
     return Item("Смартфон", 10000, 20)
 
 
 def test_item(item) :
+    """Тест атрибутов"""
     assert item.name == "Смартфон"
     assert item.price == 10000
     assert item.quantity == 20
 
 
 def test_calculate_total_price(item):
+    """Тест метода общей стоимости товара"""
     assert item.calculate_total_price() == 200000
 
 
 def test_apply_discount(item):
+    """ Тест метода применения скидки"""
     item.pay_rate = 0.8
     item.apply_discount()
     assert item.price == 8000.0
